74ecba89146ae54bbdb2d215d0c4a66c492d8e35
katsyarina789 <katsyarina.fedorova97@gmail.com>
2019-08-27 02:36:31 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double sum = 1000;
+        double proc = 17.0;
+        int years = 2;
+
+        for (int i = 1; i<years; i++){
+
+            sum += (sum*(proc/100)*years);}
+
+        System.out.println(sum);
+
+    }
+}
